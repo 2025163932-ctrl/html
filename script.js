16fced95731e9d18d48219d6e605d338d5b3a456
AvarageHobo <2025163932@ub.edu.bz>
2025-10-30 13:59:31 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-// script.js - custom scripts placeholder
+var inputOneIsOn = false;
+var inputTwoIsOn = false;
+
+// Toggle Input 1 Button
+function toggleImage() {
+    var imgElement = document.getElementById('toggleImage');
+    if (imgElement.src.includes("offbutton.png")) {
+        imgElement.src = "onbutton.png";
+        inputOneIsOn = true;
+    } else {
+        imgElement.src = "offbutton.png";
+        inputOneIsOn = false;
+    }
+}
+
+// Toggle Input 2 Button
+function toggleImage2() {
+    var imgElement = document.getElementById('toggleImage2');
+    if (imgElement.src.includes("offbutton.png")) {
+        imgElement.src = "onbutton.png";
+        inputTwoIsOn = true;
+    } else {
+        imgElement.src = "offbutton.png";
+        inputTwoIsOn = false;
+    }
+}
+
+// AND Gate Logic (updates only when "Run" is pressed)
+function and() {
+    var andGateImage = document.getElementById('andGate');
+
+    if (inputOneIsOn && inputTwoIsOn) {
+        andGateImage.src = 'and2on.png';
+    } else if (!inputOneIsOn && inputTwoIsOn) {
+        andGateImage.src = 'andoffon.png';
+    } else if (inputOneIsOn && !inputTwoIsOn) {
+        andGateImage.src = 'andonoff.png';
+    } else {
+        andGateImage.src = 'and2off.png';
+    }
+}
+
+
+
